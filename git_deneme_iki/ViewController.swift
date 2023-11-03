9bcb78ef7ce8b16e0f3236bad304d48c91944e98
a1uslu <a1uslu@icloud.com>
2023-11-04 00:04:08 +0300

    Initial Commit

---
+++ git_deneme_iki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git_deneme_iki
+//
+//  Created by Aydın Uslu on 4.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
